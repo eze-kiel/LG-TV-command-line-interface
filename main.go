6fd02bdc5496469490b60f042818a25f6f9fb9a3
Hugo <hugblanc69@gmail.com>
2019-07-19 18:04:13 +0200

    you can now change the volume value

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"net"
 	"strings"
-	//"os"
 )
 
 //define the IP and the port used by the TV on the network
@@ -16,12 +15,13 @@ const serverPort = "9761" //by default, this is the port used by LG
 //Manage command line arguments
 var (
 	mute = flag.String("mute", "", "Boolean value : true ==  muted")
+	volume = flag.Int("volume", 200, "Apply the volume value : must be between 0 and 100")
 )
 
 func main() {
 	flag.Parse()
 
-	fmt.Println("muted:", *mute)
+	fmt.Println(*volume)
 
 	if *mute != "" {
 		if *mute == "true" {
@@ -29,6 +29,12 @@ func main() {
 		} else {
 			startClient(fmt.Sprintf("%s:%s", serverHost, serverPort), "ke 00 01\n")
 		}
+	}
+
+	if !(*volume < 0 || *volume > 100) {
+		volumeInHexa := fmt.Sprintf("%x", *volume)
+
+		startClient(fmt.Sprintf("%s:%s", serverHost, serverPort), fmt.Sprintf("kf 00 %s\n", volumeInHexa))
 	}
 
 }
