9dd153a9cc3ce9e3c1c371277599fd0a7c54908a
eze-kiel <hugoblanc@fastmail.com>
2020-08-10 12:48:48 +0200

    updated main.go

--- main.go
+++ main.go
@@ -3,43 +3,25 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"github.com/urfave/cli"
 )
 
-//this type represent a variadic func which returns a string and the error
-type fn func(...string) (string, error)
-
-/*
-TODO:
- -server
- -port
- -loglevel
-*/
-
-var m map[string]fn
-
-//This map uses the fn type introduced before
-//We create a map called m which associates a string keyword (between [])
-// with a function which is build following the fn type
-func initializeCommands() {
-	m = map[string]fn{
-		"mute":       mute,
-		"volume":     volume,
-		"poweroff":   poweroff,
-		"input":      input,
-		"contrast":   contrast,
-		"brightness": brightness,
-	}
-}
+var serverHost, serverPort string
 
 func main() {
-	log.SetLevel(log.DebugLevel)
+	app := cli.NewApp()
+	app.Name = "LG TV command-line interface"
+	app.Usage = "Let's you drive your LG TV from your terminal!"
+	app.EnableBashCompletion = true
 
 	// set the viper options to read config.yaml
 	viper.SetConfigName("config")
@@ -52,109 +34,77 @@ func main() {
 		log.Fatalf("Impossible to read conf file: %v", err)
 	}
 
-	serverHost := inConfigFile("ip").(string)
-	serverPort := inConfigFile("port").(string)
+	serverHost = inConfigFile("ip").(string)
+	serverPort = inConfigFile("port").(string)
 
-	initializeCommands()
+	app.Commands = []cli.Command{
+		{
+			Name:  "volume",
+			Usage: "Adjusts the TV volume. The value must be in 0-100",
+			Action: func(c *cli.Context) error {
+				volVal := c.Args().First()
+				volValInt, _ := strconv.Atoi(volVal)
+				err := sendCommand(serverHost, serverPort, fmt.Sprintf("kf 00 %.2x", volValInt))
+				if err != nil {
+					return err
+				}
 
-	// f represents a function associated with the first argument given to the program
-	// so by entering "mute" as first arg, thanks to the m map, f represents
-	// the function mute(...string) (string,error)
-	f := m[os.Args[1]]
-	if f == nil {
-		fmt.Printf("Error: unable to find command %s", os.Args[1])
+				return nil
+			},
+		},
+		{
+			Name:  "mute",
+			Usage: "Turns on/off the TV sound. The argument must be true to mute, anything else to unmute",
+			Action: func(c *cli.Context) error {
+				if strings.ToLower(c.Args().First()) == "true" || c.Args().First() == "1" {
+					// mute the sound
+					err := sendCommand(serverHost, serverPort, "ke 00 00")
+					if err != nil {
+						return err
+					}
+					return nil
+				}
+
+				// unmute the sound
+				err := sendCommand(serverHost, serverPort, "ke 00 01")
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "poweroff",
+			Usage: "Turns off the TV. Needs no arguments",
+			Action: func(c *cli.Context) error {
+				err := sendCommand(serverHost, serverPort, "ka 00 00")
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "brightness",
+			Usage: "Adjusts the brightness of the screen. The value must be in 0-100",
+			Action: func(c *cli.Context) error {
+				brightVal := c.Args().First()
+				brightValInt, _ := strconv.Atoi(brightVal)
+				err := sendCommand(serverHost, serverPort, fmt.Sprintf("kh 00 %.2x", brightValInt))
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		},
 	}
 
-	// the f function is applied with the second arg given to program. It should be a value or a state to
-	// apply to the TV (e.g the volume value, the brightness...)
-	command, err := f(os.Args[2:]...)
+	// Sort commands list in help panel by name
+	sort.Sort(cli.CommandsByName(app.Commands))
 
+	err = app.Run(os.Args)
 	if err != nil {
-		log.Fatalf(`Error: unable retrieve command for "%s"`, os.Args[1])
-	}
-
-	// the sendCommand function is used. This one just send the command to the server (TV)
-	err = sendCommand(serverHost, serverPort, command)
-
-	if err != nil {
-		log.Error("Failed to send command: ", err)
-	}
-}
-
-//This function send a command string to the IP address given at the port given
-func sendCommand(srv string, port string, command string) error {
-
-	// add the command to a new reader for io and append an necessited carriage return
-	r := strings.NewReader(command + "\n")
-
-	log.Debugf("connecting to TV at %s:%s", srv, port)
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", srv, port))
-	if err != nil {
-		fmt.Printf("Can't connect to server: %s\n", err)
-		return nil
-	}
-
-	log.Debugf(`sending command "%s" to TV`, command)
-
-	// copy the reader in the io, so this "send" it to the server (copy-paste to the i buffer)
-	_, err = io.Copy(conn, r)
-
-	if err != nil {
-		return fmt.Errorf("Connection error: %d", err)
-	}
-
-	return nil
-}
-
-// set the mute setting (true/false)
-func mute(vals ...string) (string, error) {
-	if vals[0] == "" {
-		return "", fmt.Errorf("invalid mute value provided: %s", vals[0])
-	}
-
-	if vals[0] == "true" {
-		// mute the sound
-		return "ke 00 00", nil
-	}
-
-	// unmute the sound
-	return "ke 00 01", nil
-}
-
-// set the volume (0-100)
-func volume(vals ...string) (string, error) {
-	// convert the string into a int
-	value, err := strconv.ParseInt(vals[0], 10, 64)
-
-	if err != nil {
-		return "", err
-	}
-
-	if value < 0 || value > 100 {
-		return "", fmt.Errorf("invalid value: %d", value)
-	}
-	// append the variable part of the function to the fixed one
-	return fmt.Sprintf("kf 00 %.2x", value), nil
-}
-
-// turn off the screen (no param)
-func poweroff(vals ...string) (string, error) {
-	return "ka 00 00", nil
-}
-
-func input(vals ...string) (string, error) {
-	switch strings.ToLower(vals[0]) {
-	case "hdmi1":
-		return "xb 00 a0", nil
-
-	case "rgb":
-		return "xb 00 60", nil
-
-	case "dvi":
-		return "xb 00 70", nil
-
-	default:
-		return "", fmt.Errorf("No input found")
+		log.Fatal(err)
 	}
 }
 
@@ -166,32 +116,27 @@ func inConfigFile(param string) interface{} {
 	return viper.Get(param)
 }
 
-func contrast(vals ...string) (string, error) {
-	// convert the string into a int
-	value, err := strconv.ParseInt(vals[0], 10, 64)
+//This function sends a command string to the IP address given at the given port
+func sendCommand(srv string, port string, command string) error {
+
+	// add the command to a new reader for io and append an necessited carriage return
+	r := strings.NewReader(command + "\n")
+
+	logrus.Debugf("connecting to TV at %s:%s", srv, port)
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", srv, port))
+	if err != nil {
+		fmt.Printf("Can't connect to server: %s\n", err)
+		return nil
+	}
+
+	logrus.Debugf("sending command %s to TV", command)
+
+	// copy the reader in the io, so this "send" it to the server (copy-paste to the i buffer)
+	_, err = io.Copy(conn, r)
 
 	if err != nil {
-		return "", err
+		return fmt.Errorf("Connection error: %d", err)
 	}
 
-	if value < 0 || value > 100 {
-		return "", fmt.Errorf("invalid value: %d", value)
-	}
-	// append the variable part of the function to the fixed one
-	return fmt.Sprintf("kg 00 %.2x", value), nil
-}
-
-func brightness(vals ...string) (string, error) {
-	// convert the string into a int
-	value, err := strconv.ParseInt(vals[0], 10, 64)
-
-	if err != nil {
-		return "", err
-	}
-
-	if value < 0 || value > 100 {
-		return "", fmt.Errorf("invalid value: %d", value)
-	}
-	// append the variable part of the function to the fixed one
-	return fmt.Sprintf("kh 00 %.2x", value), nil
+	return nil
 }
